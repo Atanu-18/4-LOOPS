453425468ada52f75168bf934dba9da063b52a63
atanu mandal <atanu2697@gmail.com>
2025-04-24 13:04:00 +0530

    task update

---
+++ task1.java
@@ -0,0 +1,21 @@
+//Q. print all even numbers till n 
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no of (n):");
+        int n = sc.nextInt();
+
+        System.out.println("Even no q to " + n + " are:");
+
+        for(int i = 2; i <= n; i += 2) {
+            System.out.println(i + " ");
+        }
+
+
+        sc.close();
+    }
+}
