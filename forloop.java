dbd09b7fe125a907733089cec24d3fa282dfc637
atanu mandal <atanu2697@gmail.com>
2025-04-24 11:57:16 +0530

    4 loops

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class forloop {
+
+    public static void main(String[] args) {
+        for(int counter = 0; counter < 3; counter++) {             //counter++ ==> counter = counter + 1
+            System.out.println("Hello World");
+        }
+    }
+}
+
+//counter < 3 ====> if it is removed from for loop then it is called infinite loop-----
